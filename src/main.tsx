a183ebb6f81533adc8249edfb4242500c728caf0
Fran <fducekuce@gmail.com>
2023-11-13 22:07:45 +0100

    Create footer component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 import Navbar from "./components/Navbar.tsx"
+import Footer from "./components/Footer.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Navbar />
     <App />
+    <Footer />
   </React.StrictMode>
 )
