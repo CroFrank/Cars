4f74c00a54d9d57f659e96180e05ebe5df9f0154
Fran <fducekuce@gmail.com>
2023-11-13 22:31:26 +0100

    create AnchorTag component

---
+++ src/components/AnchorTag.tsx
@@ -0,0 +1,23 @@
+interface AnchorTagProps {
+  location: string
+  style: "create" | "edit" | "cancel"
+  children: string
+}
+
+const styles = {
+  create: "bg-green-700 hover:bg-green-600",
+  edit: "bg-blue-700 hover:bg-blue-600",
+  cancel: "bg-red-700 hover:bg-red-600",
+}
+
+export default function AnchorTag({
+  location,
+  style,
+  children,
+}: AnchorTagProps) {
+  return (
+    <a href={location} className={`${styles[style]} px-4 py-2 rounded`}>
+      {children}
+    </a>
+  )
+}
