e38a726ba541aaf2aa0fb7b124e87060a2f74deb
Fran <fducekuce@gmail.com>
2023-11-13 22:32:33 +0100

    update anchor in Navbar to AnchorTag component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,11 @@
+import AnchorTag from "./AnchorTag"
+
 export default function Navbar() {
   return (
     <div className="bg-gray-800 text-white">
       <nav className="container mx-auto flex items-center justify-between p-4">
         <h2 className="text-2xl font-bold">Cars</h2>
-        <a
-          href="#"
-          className="bg-green-700 hover:bg-green-600 px-4 py-2 rounded"
-        >
-          New Car
-        </a>
+        <AnchorTag location="#" style="create" children="New Car" />
       </nav>
     </div>
   )
