fd1d0b687059900a75963d6d08b2d98d89854bb2
Fran <fducekuce@gmail.com>
2023-11-14 13:19:59 +0100

    Create New page

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import App from "./pages/Home.tsx"
 import "./index.css"
 import Navbar from "./components/Navbar.tsx"
 import Footer from "./components/Footer.tsx"
-import CarDetails from "./pages/CarDetails.tsx"
+import New from "./pages/New.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <CarDetails />
+      <New />
       <Footer />
     </div>
   </React.StrictMode>
---
+++ src/pages/New.tsx
@@ -0,0 +1,71 @@
+import AnchorTagBtn from "../components/AnchorTagBtn"
+
+export default function New() {
+  return (
+    <div className="container mx-auto flex-grow p-4">
+      <h2 className="mx-auto mt-8 mb-12 text-center font-semibold text-yellow-600 text-2xl">
+        Create new car model
+      </h2>
+      <form
+        action="#"
+        method="post"
+        className="max-w-md mx-auto my-8 p-4 border-2 rounded-md shadow-md"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="carModel"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Car Model
+          </label>
+          <input
+            type="text"
+            id="carModel"
+            name="carModel"
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="carBrand"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Car Brand
+          </label>
+          <input
+            type="text"
+            id="carBrand"
+            name="carBrand"
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="price"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Price
+          </label>
+          <input
+            type="text"
+            id="price"
+            name="price"
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+
+        <div className="flex justify-between">
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-500 text-white rounded-md"
+          >
+            Create
+          </button>
+          <AnchorTagBtn location="#" style="cancel" children="Cancel" />
+        </div>
+      </form>
+    </div>
+  )
+}
